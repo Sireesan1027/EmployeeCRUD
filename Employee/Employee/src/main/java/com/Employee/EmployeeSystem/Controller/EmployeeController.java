41baa9061ec599d351098a7552ecf9217aeee704
ASUS <srikuna127@gmail.com>
2023-04-09 07:33:48 +0530

    Add Validation For Employee Entity and Set The Logger For Controller Class

--- Employee/Employee/src/main/java/com/Employee/EmployeeSystem/Controller/EmployeeController.java
+++ Employee/Employee/src/main/java/com/Employee/EmployeeSystem/Controller/EmployeeController.java
@@ -2,6 +2,9 @@ package com.Employee.EmployeeSystem.Controller;
 
 import com.Employee.EmployeeSystem.entity.Employee;
 import com.Employee.EmployeeSystem.service.EmployeeServiceImpl;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.Employee.EmployeeSystem.service.EmployeeService;
@@ -13,37 +16,45 @@ public class EmployeeController {
    // @RequestMapping(value="/hello",method= RequestMethod.GET)
     @Autowired
     private EmployeeService employeeService;
+
+    private final Logger LOGGER= LoggerFactory.getLogger(EmployeeController.class);
     @GetMapping("/hello")
     public String HelloWorld(){
-       return "Welcome to Employee";
+     return "Welcome to Employee";
     }
 
     @PostMapping("/Employee")
-    public Employee SaveEmployee(@RequestBody Employee employee){
+    public Employee SaveEmployee(@Valid @RequestBody Employee employee){
+     LOGGER.info("Inside SaveEmployee of Employee Controller");
      return employeeService.SaveEmployee(employee);
     }
     @GetMapping("/Employee")
     public List<Employee>fetchEmployeeList(){
+     LOGGER.info("Inside fetchEmployeeList of Employee Controller");
      return employeeService.fatchEmployeeList();
     }
 
     @GetMapping("/Employee/{id}")
     public List<Employee>fetchEmployeeListById(@PathVariable("id") long employeeId){
+     LOGGER.info("Inside fetchEmployeeListById of Employee Controller");
     return employeeService.fatchEmployeeListById(employeeId);
  }
     @DeleteMapping ("/Employee/{id}")
      public String deleteEmployeeById(@PathVariable("id") long employeeId){
+     LOGGER.info("Inside deleteEmployeeById of Employee Controller");
      employeeService.deleteEmployeeById(employeeId);
      return "Employee Deleted Sucessfully";
      }
      @PutMapping("/Employee/{id}")
      public Employee updateEmployeeById(@PathVariable("id") long employeeId,@RequestBody Employee employee){
+       LOGGER.info("Inside updateEmployeeById of Employee Controller");
        return employeeService.updateEmployeeById(employeeId,employee);
      }
 
      //Implement fatchEmployeeListByName
      @GetMapping("/Employee/name/{name}")
-     public Employee fetchEmployeeListById(@PathVariable("name") String employeeName){
+     public Employee fetchEmployeeListByName(@PathVariable("name") String employeeName){
+      LOGGER.info("Inside fetchEmployeeListByName of Employee Controller");
      return employeeService.fatchEmployeeListByName(employeeName);
      }
 }
--- Employee/Employee/src/main/java/com/Employee/EmployeeSystem/entity/Employee.java
+++ Employee/Employee/src/main/java/com/Employee/EmployeeSystem/entity/Employee.java
@@ -4,57 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Entity
+@Data
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long employeeId;
+    @NotBlank(message = "Please Add Employee Name")
     private String empName;
     private String email;
 
-    public Employee() {
-    }
 
-    public Employee(long employeeId, String empName, String email) {
-        this.employeeId = employeeId;
-        this.empName = empName;
-        this.email = email;
-    }
-
-    public long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmpName() {
-        return empName;
-    }
-
-    public void setEmpName(String empName) {
-        this.empName = empName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "EmployeeId=" + employeeId +
-                ", EmpName='" + empName + '\'' +
-                ", Email='" + email + '\'' +
-                '}';
-    }
 }
